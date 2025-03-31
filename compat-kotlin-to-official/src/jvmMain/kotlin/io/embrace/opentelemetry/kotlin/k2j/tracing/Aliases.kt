8c6ca3a9a59b7e8b579e4af862943f4ac3e5d801
Jamie Lynch <fractalwrench@gmail.com>
2025-03-31 13:44:11 +0100

    add typealiases for otel java classes

---
+++ compat-kotlin-to-official/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/tracing/Aliases.kt
@@ -0,0 +1,29 @@
+package io.embrace.opentelemetry.kotlin.k2j.tracing
+
+import io.opentelemetry.api.common.AttributeKey
+import io.opentelemetry.api.common.Attributes
+import io.opentelemetry.api.internal.ImmutableSpanContext
+import io.opentelemetry.api.trace.Span
+import io.opentelemetry.api.trace.SpanContext
+import io.opentelemetry.api.trace.SpanKind
+import io.opentelemetry.api.trace.StatusCode
+import io.opentelemetry.api.trace.TraceFlags
+import io.opentelemetry.api.trace.TraceState
+import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.api.trace.TracerProvider
+import io.opentelemetry.context.Context
+import io.opentelemetry.sdk.common.Clock
+
+internal typealias OtelJavaAttributes = Attributes
+internal typealias OtelJavaAttributeKey<T> = AttributeKey<T>
+internal typealias OtelJavaSpan = Span
+internal typealias OtelJavaSpanContext = SpanContext
+internal typealias OtelJavaTraceFlags = TraceFlags
+internal typealias OtelJavaTraceState = TraceState
+internal typealias OtelJavaSpanKind = SpanKind
+internal typealias OtelJavaStatusCode = StatusCode
+internal typealias OtelJavaContext = Context
+internal typealias OtelJavaTracer = Tracer
+internal typealias OtelJavaTracerProvider = TracerProvider
+internal typealias OtelJavaClock = Clock
+internal typealias OtelJavaImmutableSpanContext = ImmutableSpanContext
