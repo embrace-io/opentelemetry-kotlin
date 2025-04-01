6013bd409b5d212affc2fea855004132dd9a6c6a
Jamie Lynch <fractalwrench@gmail.com>
2025-04-01 15:01:59 +0100

    log interface

--- compat-kotlin-to-official/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/tracing/TracerProviderAdapter.kt
+++ compat-kotlin-to-official/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/tracing/TracerProviderAdapter.kt
@@ -1,6 +1,7 @@
 package io.embrace.opentelemetry.kotlin.k2j.tracing
 
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import io.embrace.opentelemetry.kotlin.attributes.AttributeContainer
 import io.embrace.opentelemetry.kotlin.k2j.ClockAdapter
 import io.embrace.opentelemetry.kotlin.tracing.Tracer
 import io.embrace.opentelemetry.kotlin.tracing.TracerProvider
@@ -11,7 +12,12 @@ public class TracerProviderAdapter(
     private val clock: ClockAdapter = ClockAdapter()
 ) : TracerProvider {
 
-    override fun getTracer(name: String, version: String?): Tracer {
+    override fun getTracer(
+        name: String,
+        version: String?,
+        schemaUrl: String?,
+        attributes: AttributeContainer.() -> Unit
+    ): Tracer {
         val tracer = when (version) {
             null -> tracerProvider.get(name)
             else -> tracerProvider.get(name, version)
