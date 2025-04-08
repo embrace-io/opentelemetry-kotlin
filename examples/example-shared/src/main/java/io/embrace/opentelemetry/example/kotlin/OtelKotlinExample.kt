14ffba8e5a3ed6bb19c903d03ba676b133161cc6
Jamie Lynch <fractalwrench@gmail.com>
2025-04-08 09:16:04 +0100

    update example to use kotlin api

--- examples/example-shared/src/main/java/io/embrace/opentelemetry/example/kotlin/OtelKotlinExample.kt
+++ examples/example-shared/src/main/java/io/embrace/opentelemetry/example/kotlin/OtelKotlinExample.kt
@@ -1,15 +1,30 @@
 package io.embrace.opentelemetry.example.kotlin
 
+import io.embrace.opentelemetry.example.ExampleLogRecordProcessor
+import io.embrace.opentelemetry.example.ExampleSpanProcessor
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import io.embrace.opentelemetry.kotlin.OpenTelemetry
+import io.embrace.opentelemetry.kotlin.k2j.OpenTelemetrySdk
+import io.opentelemetry.sdk.logs.SdkLoggerProvider
+import io.opentelemetry.sdk.trace.SdkTracerProvider
+
+@OptIn(ExperimentalApi::class)
+private fun instantiateOtelApi(): OpenTelemetry {
+    val otelJava = io.opentelemetry.sdk.OpenTelemetrySdk.builder()
+        .setTracerProvider(SdkTracerProvider.builder().addSpanProcessor(ExampleSpanProcessor()).build())
+        .setLoggerProvider(SdkLoggerProvider.builder().addLogRecordProcessor(ExampleLogRecordProcessor()).build())
+        .build()
+    return OpenTelemetrySdk(otelJava)
+}
 
 @OptIn(ExperimentalApi::class)
 fun createInstrumentationWithOtelKotlin() {
-    val api = OtelKotlinApi()
-    val tracer = api.tracer.getTracer(
+    val api = instantiateOtelApi()
+    val tracer = api.tracerProvider.getTracer(
         name = "kotlin-example-app",
         version = "0.1.0"
     )
     tracer.createSpan("my_span").end()
-    api.logger.logRecordBuilder().setBody("Hello, World!").emit()
+    api.loggerProvider.getLogger("my_logger").log("Hello, World!")
 }
 
