a8552a4e7b0a6f8365131ad79c0b0e2d5766dacf
Francisco Prieto <priettt@gmail.com>
2025-06-09 17:39:13 -0300

    Add tests using java junit4 testing extension

--- examples/telescope-app/app/build.gradle.kts
+++ examples/telescope-app/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
+    testImplementation(libs.opentelemetry.sdk.testing)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
