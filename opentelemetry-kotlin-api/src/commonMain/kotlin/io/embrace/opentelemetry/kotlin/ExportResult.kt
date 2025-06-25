c7a6755a0ffb1c376cdff3d0c2d4ed7a9a8c0ed3
Jamie Lynch <fractalwrench@gmail.com>
2025-06-25 17:12:08 +0100

    api: begin creating exporters api

---
+++ opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/ExportResult.kt
@@ -0,0 +1,9 @@
+package io.embrace.opentelemetry.kotlin
+
+/**
+ * Whether the export operation was successful or not.
+ */
+public enum class ExportResult {
+    SUCCESS,
+    FAILURE,
+}
---
+++ opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/logging/export/LogRecordData.kt
@@ -0,0 +1,9 @@
+package io.embrace.opentelemetry.kotlin.logging.export
+
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
+
+/**
+ * An immutable representation of a log record that is ready for export.
+ */
+@ExperimentalApi
+public interface LogRecordData // TODO: add required properties.
