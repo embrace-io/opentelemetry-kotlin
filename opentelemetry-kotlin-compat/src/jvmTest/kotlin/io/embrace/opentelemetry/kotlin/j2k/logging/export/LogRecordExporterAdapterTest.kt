129db760f5adf16c678a07ec85973845b192c5fe
Jamie Lynch <fractalwrench@gmail.com>
2025-07-03 12:04:37 +0100

    create mapping functions for span/log exporters

--- opentelemetry-kotlin-compat/src/jvmTest/kotlin/io/embrace/opentelemetry/kotlin/j2k/logging/export/LogRecordExporterAdapterTest.kt
+++ opentelemetry-kotlin-compat/src/jvmTest/kotlin/io/embrace/opentelemetry/kotlin/j2k/logging/export/LogRecordExporterAdapterTest.kt
@@ -1,11 +1,15 @@
 package io.embrace.opentelemetry.kotlin.j2k.logging.export
 
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.export.OperationResultCode
 import io.embrace.opentelemetry.kotlin.fakes.otel.java.FakeOtelJavaLogRecordExporter
+import io.embrace.opentelemetry.kotlin.fakes.otel.kotlin.FakeReadableLogRecord
+import io.embrace.opentelemetry.kotlin.k2j.tracing.toMap
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
+@OptIn(ExperimentalApi::class)
 internal class LogRecordExporterAdapterTest {
 
     private lateinit var impl: FakeOtelJavaLogRecordExporter
@@ -31,6 +35,24 @@ internal class LogRecordExporterAdapterTest {
 
     @Test
     fun `test export`() {
-        TODO("Not yet implemented")
+        val original = FakeReadableLogRecord()
+        assertEquals(OperationResultCode.Success, wrapper.export(listOf(original)))
+
+        val observed = impl.exports.single()
+        assertEquals(original.timestamp, observed.timestampEpochNanos)
+        assertEquals(original.observedTimestamp, observed.observedTimestampEpochNanos)
+        assertEquals(original.severityText, observed.severityText)
+        assertEquals(original.severityNumber?.severityNumber, observed.severity.severityNumber)
+        assertEquals(original.body, observed.bodyValue?.asString())
+        assertEquals(original.attributes, observed.attributes.toMap())
+        assertEquals(original.resource?.attributes, observed.resource.attributes.toMap())
+        assertEquals(original.spanContext.spanId, observed.spanContext.spanId)
+
+        val originalScope = original.instrumentationScopeInfo
+        val observedScope = observed.instrumentationScopeInfo
+        assertEquals(originalScope?.name, observedScope.name)
+        assertEquals(originalScope?.version, observedScope.version)
+        assertEquals(originalScope?.schemaUrl, observedScope.schemaUrl)
+        assertEquals(originalScope?.attributes, observedScope.attributes.toMap())
     }
 }
