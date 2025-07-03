0786a33c391285e7c793d9c60228336d12bc3e2c
Jamie Lynch <fractalwrench@gmail.com>
2025-07-03 14:05:39 +0100

    begin implementing kotlin DSL

--- opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/OpenTelemetryInstanceK2J.kt
+++ opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/OpenTelemetryInstanceK2J.kt
@@ -1,10 +1,15 @@
 package io.embrace.opentelemetry.kotlin
 
+import io.embrace.opentelemetry.kotlin.aliases.OtelJavaClock
 import io.embrace.opentelemetry.kotlin.aliases.OtelJavaOpenTelemetry
 import io.embrace.opentelemetry.kotlin.init.LoggerProviderConfigDsl
 import io.embrace.opentelemetry.kotlin.init.TracerProviderConfigDsl
 import io.embrace.opentelemetry.kotlin.k2j.ClockAdapter
 import io.embrace.opentelemetry.kotlin.k2j.OpenTelemetrySdk
+import io.embrace.opentelemetry.kotlin.k2j.init.LoggerProviderConfigImpl
+import io.embrace.opentelemetry.kotlin.k2j.init.TracerProviderConfigImpl
+import io.embrace.opentelemetry.kotlin.k2j.logging.LoggerProviderAdapter
+import io.embrace.opentelemetry.kotlin.k2j.tracing.TracerProviderAdapter
 
 /**
  * Constructs an [OpenTelemetry] instance that decorates the OpenTelemetry Java SDK. This will not use the Kotlin
@@ -12,16 +17,26 @@ import io.embrace.opentelemetry.kotlin.k2j.OpenTelemetrySdk
  * OpenTelemetry Java SDK code that you don't want to rewrite, but still wish to use the Kotlin API.
  */
 @ExperimentalApi
-public fun OpenTelemetryInstance.compatWithOtelJava(impl: OtelJavaOpenTelemetry): OpenTelemetry = OpenTelemetrySdk(impl)
+public fun OpenTelemetryInstance.compatWithOtelJava(impl: OtelJavaOpenTelemetry): OpenTelemetry =
+    OpenTelemetrySdk(
+        tracerProvider = TracerProviderAdapter(impl.tracerProvider),
+        loggerProvider = LoggerProviderAdapter(impl.logsBridge),
+        clock = ClockAdapter(OtelJavaClock.getDefault())
+    )
 
 /**
  * Constructs an [OpenTelemetry] instance by using the Kotlin API DSL to configure an underlying OpenTelemetry Java SDK
  * instance.
  */
-@Suppress("UNUSED_PARAMETER")
 @ExperimentalApi
 public fun OpenTelemetryInstance.kotlinApi(
     tracerProvider: TracerProviderConfigDsl.() -> Unit = {},
     loggerProvider: LoggerProviderConfigDsl.() -> Unit = {},
     clock: Clock = ClockAdapter(io.opentelemetry.sdk.common.Clock.getDefault()),
-): OpenTelemetry = throw UnsupportedOperationException()
+): OpenTelemetry {
+    return OpenTelemetrySdk(
+        tracerProvider = TracerProviderConfigImpl(clock).apply(tracerProvider).build(),
+        loggerProvider = LoggerProviderConfigImpl(clock).apply(loggerProvider).build(),
+        clock = clock
+    )
+}
---
+++ opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/init/LoggerProviderConfigImpl.kt
@@ -0,0 +1,36 @@
+package io.embrace.opentelemetry.kotlin.k2j.init
+
+import io.embrace.opentelemetry.kotlin.Clock
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import io.embrace.opentelemetry.kotlin.aliases.OtelJavaSdkLoggerProvider
+import io.embrace.opentelemetry.kotlin.attributes.AttributeContainer
+import io.embrace.opentelemetry.kotlin.init.LoggerProviderConfigDsl
+import io.embrace.opentelemetry.kotlin.k2j.logging.LoggerProviderAdapter
+import io.embrace.opentelemetry.kotlin.k2j.tracing.AttributeContainerImpl
+import io.embrace.opentelemetry.kotlin.logging.LoggerProvider
+import io.embrace.opentelemetry.kotlin.logging.export.LogRecordProcessor
+import io.opentelemetry.sdk.logs.SdkLoggerProviderBuilder
+import io.opentelemetry.sdk.resources.Resource
+
+@ExperimentalApi
+internal class LoggerProviderConfigImpl(
+    clock: Clock
+) : LoggerProviderConfigDsl {
+
+    private val builder: SdkLoggerProviderBuilder = OtelJavaSdkLoggerProvider.builder()
+
+    init {
+        builder.setClock(OtelJavaClockWrapper(clock))
+    }
+
+    override fun resource(action: AttributeContainer.() -> Unit) {
+        val attrs = AttributeContainerImpl().apply(action).otelJavaAttributes()
+        builder.setResource(Resource.create(attrs))
+    }
+
+    override fun addLogRecordProcessor(processor: LogRecordProcessor) {
+        TODO("Not yet implemented")
+    }
+
+    fun build(): LoggerProvider = LoggerProviderAdapter(builder.build())
+}
