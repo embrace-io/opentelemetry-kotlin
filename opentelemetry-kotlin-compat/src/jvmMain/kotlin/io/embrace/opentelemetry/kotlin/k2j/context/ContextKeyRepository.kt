9392e1a5d8340bd8fa9950e21d6f2dd2dde7fd13
Jamie Lynch <fractalwrench@gmail.com>
2025-07-08 11:01:08 +0100

    fix context leak

--- opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/context/ContextKeyRepository.kt
+++ opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/context/ContextKeyRepository.kt
@@ -3,20 +3,18 @@ package io.embrace.opentelemetry.kotlin.k2j.context
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.aliases.OtelJavaContextKey
 import io.embrace.opentelemetry.kotlin.context.ContextKey
-import java.util.concurrent.ConcurrentHashMap
+import java.util.Collections
+import java.util.WeakHashMap
 
 @Suppress("UNCHECKED_CAST")
 @OptIn(ExperimentalApi::class)
 internal class ContextKeyRepository {
 
-    // TODO: future: cleanup stale references
-    // TODO: future: support nullable values
-
     companion object {
         val INSTANCE = ContextKeyRepository()
     }
 
-    private val impl = ConcurrentHashMap<OtelJavaContextKey<*>, ContextKey<*>>()
+    private val impl = Collections.synchronizedMap(WeakHashMap<OtelJavaContextKey<*>, ContextKey<*>>())
 
     fun <T> get(key: OtelJavaContextKey<T>): ContextKey<T> {
         return impl.getOrPut(key) {
