dbdcb9dc04a538f5a26865d1ddc7988e196a30fd
Jamie Lynch <fractalwrench@gmail.com>
2025-07-11 09:18:09 +0100

    refactor: attribute container extension

---
+++ opentelemetry-kotlin-api-ext/src/commonTest/kotlin/io/embrace/opentelemetry/kotlin/attributes/AttributeContainerExtTest.kt
@@ -0,0 +1,75 @@
+package io.embrace.opentelemetry.kotlin.attributes
+
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalApi::class)
+internal class AttributeContainerExtTest {
+
+    @Test
+    fun `set attributes`() {
+        val attrs = FakeAttributeContainer()
+        val input = mapOf(
+            "string" to "value",
+            "long" to 5L,
+            "double" to 10L,
+            "bool" to true,
+            "stringList" to listOf("value"),
+            "longList" to mutableListOf(5L),
+            "doubleList" to setOf(10L),
+            "boolList" to arrayOf(true),
+        )
+        val expected = mapOf(
+            "string" to "value",
+            "long" to 5L,
+            "double" to 10L,
+            "bool" to true,
+            "stringList" to listOf("value"),
+            "longList" to listOf(5L),
+            "doubleList" to listOf(10L),
+            "boolList" to listOf(true),
+        )
+        attrs.setAttributes(input)
+        val observed = attrs.attributes()
+        assertEquals(expected, observed)
+    }
+
+    private class FakeAttributeContainer(
+        private val attributes: MutableMap<String, Any> = mutableMapOf()
+    ) : AttributeContainer {
+        override fun setBooleanAttribute(key: String, value: Boolean) {
+            attributes[key] = value
+        }
+
+        override fun setStringAttribute(key: String, value: String) {
+            attributes[key] = value
+        }
+
+        override fun setLongAttribute(key: String, value: Long) {
+            attributes[key] = value
+        }
+
+        override fun setDoubleAttribute(key: String, value: Double) {
+            attributes[key] = value
+        }
+
+        override fun setBooleanListAttribute(key: String, value: List<Boolean>) {
+            attributes[key] = value
+        }
+
+        override fun setStringListAttribute(key: String, value: List<String>) {
+            attributes[key] = value
+        }
+
+        override fun setLongListAttribute(key: String, value: List<Long>) {
+            attributes[key] = value
+        }
+
+        override fun setDoubleListAttribute(key: String, value: List<Double>) {
+            attributes[key] = value
+        }
+
+        override fun attributes(): Map<String, Any> = attributes
+    }
+}
