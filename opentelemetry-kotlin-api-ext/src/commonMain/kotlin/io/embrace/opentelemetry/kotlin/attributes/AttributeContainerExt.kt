b30b75fb8767d7ce8cc2f27fd4fbaf1da30ad427
Jamie Lynch <fractalwrench@gmail.com>
2025-07-11 09:18:09 +0100

    refactor: attribute container extension

--- opentelemetry-kotlin-api-ext/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/attributes/AttributeContainerExt.kt
+++ opentelemetry-kotlin-api-ext/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/attributes/AttributeContainerExt.kt
@@ -12,8 +12,27 @@ import io.embrace.opentelemetry.kotlin.ThreadSafe
 @ExperimentalApi
 @ThreadSafe
 public fun AttributeContainer.setAttributes(attributes: Map<String, Any>) {
-    // TODO: add support for other attribute types
     attributes.forEach {
-        setStringAttribute(it.key, it.value.toString())
+        when (val input = it.value) {
+            is String -> setStringAttribute(it.key, input)
+            is Boolean -> setBooleanAttribute(it.key, input)
+            is Long -> setLongAttribute(it.key, input)
+            is Double -> setDoubleAttribute(it.key, input)
+            is Collection<*> -> handleCollection(it.key, input.toList())
+            is Array<*> -> handleCollection(it.key, input.toList())
+            else -> setStringAttribute(it.key, it.value.toString())
+        }
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+@OptIn(ExperimentalApi::class)
+private fun AttributeContainer.handleCollection(key: String, input: List<*>) {
+    when {
+        input.all { it is String } -> setStringListAttribute(key, input as List<String>)
+        input.all { it is Boolean } -> setBooleanListAttribute(key, input as List<Boolean>)
+        input.all { it is Double } -> setDoubleListAttribute(key, input as List<Double>)
+        input.all { it is Long } -> setLongListAttribute(key, input as List<Long>)
+        else -> return
     }
 }
