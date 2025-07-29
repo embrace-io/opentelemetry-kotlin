c7b099e5d003a2d6d01740057551fa441c796fd8
bidetofevil <hanson.ho@embrace.io>
2025-07-29 13:44:32 -0700

    Consolidate interfaces for Span to minimize duplicate definitions

--- opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/tracing/data/SpanData.kt
+++ opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/tracing/data/SpanData.kt
@@ -2,77 +2,53 @@ package io.embrace.opentelemetry.kotlin.tracing.data
 
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.InstrumentationScopeInfo
+import io.embrace.opentelemetry.kotlin.ThreadSafe
 import io.embrace.opentelemetry.kotlin.resource.Resource
-import io.embrace.opentelemetry.kotlin.tracing.model.SpanContext
-import io.embrace.opentelemetry.kotlin.tracing.model.SpanKind
 
 /**
- * Immutable representation of a Span used for exporting
+ * A full representation of a span that contains all the data needed for exporting.
  */
 @ExperimentalApi
-public interface SpanData {
-    /**
-     * The span name
-     */
-    public val name: String
-
-    /**
-     * The span status
-     */
-    public val status: StatusData
-
-    /**
-     * The parent span context.
-     */
-    public val parent: SpanContext
-
-    /**
-     * The span context that uniquely identifies this span.
-     */
-    public val spanContext: SpanContext
-
-    /**
-     * The kind of this span
-     */
-    public val spanKind: SpanKind
-
-    /**
-     * The timestamp at which this span started, in nanoseconds.
-     */
-    public val startTimestamp: Long
-
+public interface SpanData : SpanSchema {
     /**
      * The timestamp at which this span ended, in nanoseconds. If it has not ended null will return.
      */
+    @ThreadSafe
     public val endTimestamp: Long?
 
     /**
      * A map of attributes associated with the span.
      */
+    @ThreadSafe
     public val attributes: Map<String, Any>
 
     /**
      * A list of events associated with the span.
      */
+    @ThreadSafe
     public val events: List<EventData>
 
     /**
      * A list of links associated with the span.
      */
+    @ThreadSafe
     public val links: List<LinkData>
 
     /**
      * The resource associated with the object
      */
+    @ThreadSafe
     public val resource: Resource
 
     /**
      * The instrumentation scope information associated with the object
      */
+    @ThreadSafe
     public val instrumentationScopeInfo: InstrumentationScopeInfo
 
     /**
      * Returns true if this span has ended.
      */
+    @ThreadSafe
     public val hasEnded: Boolean
 }
