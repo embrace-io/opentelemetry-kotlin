6e1938cce91ecfb1bf20cc553d0cf983f6f46865
Jamie Lynch <fractalwrench@gmail.com>
2025-07-31 15:05:44 +0100

    refactor: rationalise conversion functions

---
+++ opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/k2j/tracing/ContextKeyConversion.kt
@@ -0,0 +1,9 @@
+package io.embrace.opentelemetry.kotlin.k2j.tracing
+
+import io.embrace.opentelemetry.kotlin.ExperimentalApi
+import io.embrace.opentelemetry.kotlin.aliases.OtelJavaContextKey
+import io.embrace.opentelemetry.kotlin.context.ContextKey
+import io.embrace.opentelemetry.kotlin.k2j.context.ContextKeyAdapter
+
+@OptIn(ExperimentalApi::class)
+public fun <T> ContextKey<T>.toOtelJava(): OtelJavaContextKey<T> = (this as ContextKeyAdapter).impl
