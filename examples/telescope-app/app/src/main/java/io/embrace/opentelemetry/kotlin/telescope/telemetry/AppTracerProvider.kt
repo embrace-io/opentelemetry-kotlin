530c18b53791ad2619b52ac57fbf721b4a863cdf
Francisco Prieto <priettt@gmail.com>
2025-08-05 19:33:23 -0300

    Fix telescope app

--- examples/telescope-app/app/src/main/java/io/embrace/opentelemetry/kotlin/telescope/telemetry/AppTracerProvider.kt
+++ examples/telescope-app/app/src/main/java/io/embrace/opentelemetry/kotlin/telescope/telemetry/AppTracerProvider.kt
@@ -3,7 +3,7 @@ package io.embrace.opentelemetry.kotlin.telescope.telemetry
 import android.content.res.Resources
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.OpenTelemetryInstance
-import io.embrace.opentelemetry.kotlin.compatWithOtelJava
+import io.embrace.opentelemetry.kotlin.decorateJavaApi
 import io.embrace.opentelemetry.kotlin.tracing.Tracer
 import io.opentelemetry.exporter.logging.LoggingSpanExporter
 import io.opentelemetry.sdk.OpenTelemetrySdk
@@ -36,7 +36,7 @@ class AppTracerProvider(
             .setTracerProvider(sdkTracerProvider)
             .build()
 
-        val kotlinApi = OpenTelemetryInstance.compatWithOtelJava(javaSdk)
+        val kotlinApi = OpenTelemetryInstance.decorateJavaApi(javaSdk)
 
         return kotlinApi.tracerProvider.getTracer("AppTracer")
     }
--- examples/telescope-app/app/src/main/java/io/embrace/opentelemetry/kotlin/telescope/telemetry/SessionTelemetry.kt
+++ examples/telescope-app/app/src/main/java/io/embrace/opentelemetry/kotlin/telescope/telemetry/SessionTelemetry.kt
@@ -1,16 +1,20 @@
 package io.embrace.opentelemetry.kotlin.telescope.telemetry
 
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
-import io.embrace.opentelemetry.kotlin.tracing.model.Span
+import io.embrace.opentelemetry.kotlin.context.toOtelKotlinContext
 import io.embrace.opentelemetry.kotlin.tracing.Tracer
+import io.embrace.opentelemetry.kotlin.tracing.ext.storeInContext
+import io.embrace.opentelemetry.kotlin.tracing.model.Span
+import io.opentelemetry.context.Context
 
 @OptIn(ExperimentalApi::class)
 class SessionTelemetry(
-    private val tracer: Tracer
+    private val tracer: Tracer,
 ) {
     private var parentSpan: Span? = null
     private var currentChildSpan: Span? = null
     private var currentDestination = ""
+    private val context = Context.root()
 
     fun onAppStart() {
         parentSpan = createSpan("AppSession")
@@ -29,6 +33,6 @@ class SessionTelemetry(
     }
 
     private fun createSpan(name: String, parent: Span? = null): Span {
-        return tracer.createSpan(name, parent?.spanContext)
+        return tracer.createSpan(name, parent?.storeInContext(context.toOtelKotlinContext()))
     }
 } 
