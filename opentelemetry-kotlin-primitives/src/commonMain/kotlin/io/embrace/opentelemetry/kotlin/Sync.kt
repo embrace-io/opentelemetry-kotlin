2822e5fcb0a1ebf887998fba71b761b614d27693
Jamie Lynch <fractalwrench@gmail.com>
2025-08-08 07:54:14 +0100

    span locking model

--- opentelemetry-kotlin-primitives/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.kt
+++ opentelemetry-kotlin-primitives/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.kt
@@ -4,3 +4,11 @@ package io.embrace.opentelemetry.kotlin
  * Executes the block synchronously with an exclusion lock.
  */
 public expect inline fun <R> sync(lock: Any, block: () -> R): R
+
+/**
+ * Performs an operation if the condition is true, using a double-checked lock. If the condition
+ * is not true, the block is not executed.
+ *
+ * https://en.wikipedia.org/wiki/Double-checked_locking
+ */
+public expect inline fun <R> doubleCheckedLockSync(lock: Any, condition: () -> Boolean, block: () -> R)
--- opentelemetry-kotlin-primitives/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.jvm.kt
+++ opentelemetry-kotlin-primitives/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.jvm.kt
@@ -4,3 +4,17 @@ public actual inline fun <R> sync(lock: Any, block: () -> R): R =
     synchronized(lock) {
         block()
     }
+
+public actual inline fun <R> doubleCheckedLockSync(
+    lock: Any,
+    condition: () -> Boolean,
+    block: () -> R
+) {
+    if (condition()) {
+        synchronized(lock) {
+            if (condition()) {
+                block()
+            }
+        }
+    }
+}
