41a295985cde64d79dcd86f67c268ee92a002b3c
Francisco Prieto <priettt@gmail.com>
2025-08-15 16:06:03 -0300

    Implement clock.now()

--- opentelemetry-kotlin-implementation/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/clock/ClockImpl.kt
+++ opentelemetry-kotlin-implementation/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/clock/ClockImpl.kt
@@ -3,7 +3,9 @@ package io.embrace.opentelemetry.kotlin.clock
 import io.embrace.opentelemetry.kotlin.Clock
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 
+internal expect fun getCurrentTimeNanos(): Long
+
 @OptIn(ExperimentalApi::class)
 internal class ClockImpl : Clock {
-    override fun now(): Long = 0
+    override fun now(): Long = getCurrentTimeNanos()
 }
