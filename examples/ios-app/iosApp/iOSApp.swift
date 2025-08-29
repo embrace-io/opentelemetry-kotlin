14fce338efa1cc47e147cc73aa3517d20c2eae64
Jamie Lynch <fractalwrench@gmail.com>
2025-08-29 13:05:55 +0100

    chore: add example app

---
+++ examples/ios-app/iosApp/iOSApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import OtelKotlinApi
+import OtelKotlinImplementation
+
+@main
+struct iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("SwiftUI: Hello World!")
+        }
+    }
+}
