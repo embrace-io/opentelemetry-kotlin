a5df23b2171c59175acc7a46caedf9a824d18c4c
Jamie Lynch <fractalwrench@gmail.com>
2025-09-02 13:20:30 +0100

    poc: sketch out context attachment api

--- opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/factory/ContextFactory.kt
+++ opentelemetry-kotlin-api/src/commonMain/kotlin/io/embrace/opentelemetry/kotlin/factory/ContextFactory.kt
@@ -2,6 +2,7 @@ package io.embrace.opentelemetry.kotlin.factory
 
 import io.embrace.opentelemetry.kotlin.ExperimentalApi
 import io.embrace.opentelemetry.kotlin.context.Context
+import io.embrace.opentelemetry.kotlin.context.ContextStorageMode
 import io.embrace.opentelemetry.kotlin.tracing.model.Span
 
 /**
@@ -14,6 +15,12 @@ public interface ContextFactory {
      * Retrieves the root Context.
      */
     public fun root(): Context
+
+    /**
+     * Retrieves the current Context from the given storage method. If no Context has been set
+     * for this storage method, the root Context is returned.
+     */
+    public fun current(storageMode: ContextStorageMode = ContextStorageMode.THREAD_LOCAL): Context
 
     /**
      * Stores a span and returns a new [Context], using a pre-defined key.
