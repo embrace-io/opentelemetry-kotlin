7452760c1ef75d9a97dd7ac2273cfc4865f47021
Jamie Lynch <fractalwrench@gmail.com>
2025-09-05 15:05:35 +0100

    build iOS target

--- opentelemetry-kotlin-primitives/src/appleMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.apple.kt
+++ opentelemetry-kotlin-primitives/src/appleMain/kotlin/io/embrace/opentelemetry/kotlin/Sync.apple.kt
@@ -8,4 +8,3 @@ public actual class ReentrantReadWriteLock {
     public actual fun <T> write(action: () -> T): T = throw UnsupportedOperationException()
     public actual fun <T> read(action: () -> T): T = throw UnsupportedOperationException()
 }
-
