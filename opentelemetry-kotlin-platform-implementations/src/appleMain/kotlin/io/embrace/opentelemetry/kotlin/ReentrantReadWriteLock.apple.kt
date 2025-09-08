e8f293d76467d355aebcc0670919dcd9bcc0f75c
Jamie Lynch <fractalwrench@gmail.com>
2025-09-08 16:46:33 +0100

    implement lock on apple platforms

--- opentelemetry-kotlin-platform-implementations/src/appleMain/kotlin/io/embrace/opentelemetry/kotlin/ReentrantReadWriteLock.apple.kt
+++ opentelemetry-kotlin-platform-implementations/src/appleMain/kotlin/io/embrace/opentelemetry/kotlin/ReentrantReadWriteLock.apple.kt
@@ -1,7 +1,20 @@
 package io.embrace.opentelemetry.kotlin
 
-public actual class ReentrantReadWriteLock {
-    public actual fun <T> write(action: () -> T): T = throw UnsupportedOperationException()
-    public actual fun <T> read(action: () -> T): T = throw UnsupportedOperationException()
-}
+import platform.Foundation.NSRecursiveLock
 
+public actual class ReentrantReadWriteLock {
+
+    private val lock = NSRecursiveLock()
+
+    public actual fun <T> write(action: () -> T): T {
+        lock.lock()
+        try {
+            return action()
+        } finally {
+            lock.unlock()
+        }
+    }
+
+    // take perf hit of obtaining the same lock for now, for the sake of simplicity
+    public actual fun <T> read(action: () -> T): T = write(action)
+}
