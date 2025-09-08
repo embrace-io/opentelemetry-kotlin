8323e513413683958775e4a3ea32cbb2e9457bd2
Jamie Lynch <fractalwrench@gmail.com>
2025-09-08 16:10:17 +0100

    refactor: standardise lock implementation

--- opentelemetry-kotlin-platform-implementations/src/jsMain/kotlin/io/embrace/opentelemetry/kotlin/ReentrantReadWriteLock.js.kt
+++ opentelemetry-kotlin-platform-implementations/src/jsMain/kotlin/io/embrace/opentelemetry/kotlin/ReentrantReadWriteLock.js.kt
@@ -1,7 +1,5 @@
 package io.embrace.opentelemetry.kotlin
 
-public actual inline fun <R> sync(lock: Any, block: () -> R): R = block()
-
 public actual class ReentrantReadWriteLock {
     public actual fun <T> write(action: () -> T): T = action()
     public actual fun <T> read(action: () -> T): T = action()
