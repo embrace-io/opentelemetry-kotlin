6b5e916c5e0b795e0f847f310f9eedd47a33a270
Jamie Lynch <fractalwrench@gmail.com>
2025-09-09 06:18:43 +0100

    reduce visibility

--- opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/factory/CompatSdkFactoryExt.kt
+++ opentelemetry-kotlin-compat/src/jvmMain/kotlin/io/embrace/opentelemetry/kotlin/factory/CompatSdkFactoryExt.kt
@@ -6,4 +6,4 @@ import io.embrace.opentelemetry.kotlin.ExperimentalApi
  * Creates a factory that constructs objects that work when the SDK is backed by the OTel Java SDK.
  */
 @ExperimentalApi
-public fun createCompatSdkFactory(): SdkFactory = CompatSdkFactory()
+internal fun createCompatSdkFactory(): SdkFactory = CompatSdkFactory()
